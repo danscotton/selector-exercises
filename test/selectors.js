68f5c20b89d21ebc23fa38cbc50ce6cba52f30f4
SeedBoot <oliboots@gmail.com>
2018-09-21 10:18:34 +0100

    completed no.007 :D

--- test/selectors.js
+++ test/selectors.js
@@ -1,26 +1,49 @@
-const log = o => console.log(JSON.stringify(o, null, 4))
+const log = o =>
+    console.log(JSON.stringify(o, null, 4))
+;
 
-const productsAsArray = state => Object.values(state.products);
+// -------------------------------------------------------------------------- \\
+
+const productsAsArray = state =>
+    Object.values(state.products)
+;
 
 const mappedProducts = state =>
     state.map(product => {
         const { id, name, price } = product;
         return { id, name, price };
-    });
+    })
+;
+
+// -------------------------------------------------------------------------- \\
+
+const brandList = state =>
+    productsAsArray(state).map(prod => prod.brand)
+;
+
+const brandListUnique = state =>
+    brandList(state).filter((el, index, self) => index === self.indexOf(el))
+;
+
+// -------------------------------------------------------------------------- \\
 
 // 001
 const getProducts = state =>
-    mappedProducts(productsAsArray(state));
+    mappedProducts(productsAsArray(state))
+;
 
 // 002
 const getProductsInStock = state =>
     mappedProducts(productsAsArray(state)
-        .filter(product => product.inStock));
+        .filter(product => product.inStock))
+;
 
 // 003
 const getProduct = (state, productID) =>
-    state.products[productID] ? state.products[productID] : null;
+    state.products[productID] ? state.products[productID] : null
+;
 
+//
 // 004
 const getBasket = state => Object.keys(state.basket).map(productId => {
         const { id, name, quantity, subtotal } = {
@@ -29,27 +52,32 @@ const getBasket = state => Object.keys(state.basket).map(productId => {
             quantity: state.basket[productId],
         };
         return { id, name, quantity, subtotal };
-    });
+    })
+;
 
 // 005
 const getBasketTotal = state =>
-    getBasket(state).reduce((a, b) => a.subtotal + b.subtotal);
+    getBasket(state).reduce((a, b) => a.subtotal + b.subtotal)
+;
 
-const brandList = state =>
-    productsAsArray(state)
-        .map(prod => prod.brand)
-            .filter((el, index, self) => index === self.indexOf(el));
-
+//
 // 006
 const getProductsByBrand = state =>
-    brandList(state)
-        .reduce((initialVal, cur) => ({
-            ...initialVal,
-            [cur]: [
-                ...productsAsArray(state)
-                    .filter(prod => prod.brand === cur)
-            ]
-        }), {});
+    brandListUnique(state).reduce((initialVal, cur) => ({
+        ...initialVal,
+        [cur]: [...productsAsArray(state).filter(prod => prod.brand === cur)]
+    }), {})
+;
+
+// 007
+const getBrands = state =>
+    brandList(state).sort().reduce((init, cur) => ((
+        init[init.findIndex(index => index.brand === cur)] ||
+        init[init.push({brand: cur, count: 0}) - 1]).count++, init
+        ), [])
+;
+
+// -------------------------------------------------------------------------- \\
 
 module.exports = {
     getProducts,
@@ -58,4 +86,5 @@ module.exports = {
     getBasket,
     getBasketTotal,
     getProductsByBrand,
+    getBrands,
 };
